4a38ec2a9d21217ff63b58a8383c574515309217
Fredy Nurman Saleh <email@fredyns.net>
2017-04-24 20:31:32 +0700

    add belong to behavior

---
+++ src/behaviors/BelongToModelBehavior.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace fredyns\pack\behaviors;
+
+/**
+ * Description of BelongToModelBehavior
+ *
+ * @author Fredy Nurman Saleh <email@fredyns.net>
+ */
+class BelongToModelBehavior extends \fredyns\belong2\BelongToModelBehavior
+{
+
+}

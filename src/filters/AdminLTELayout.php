4da23f6c3810424151286eac72f303e4354a40d6
Fredy Nurman Saleh <email@fredyns.net>
2017-04-24 21:17:41 +0700

    adminlte layout for logged user

---
+++ src/filters/AdminLTELayout.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace fredyns\pack\filters;
+
+use Yii;
+
+/**
+ * change layout using AdminLTE when user is logged in
+ *
+ * @author fredy
+ */
+class AdminLTELayout extends \yii\base\ActionFilter
+{
+
+    /**
+     * decide layout before execute action
+     *
+     * @param \yii\base\Action $action
+     * @return bool
+     */
+    public function beforeAction($action)
+    {
+        $main = '@app/views/layouts/main';
+        $adminlte = '@app/views/layouts/adminlte';
+
+        Yii::$app->controller->layout = (Yii::$app->user->isGuest) ? $main : $adminlte;
+
+        return parent::beforeAction($action);
+    }
+}

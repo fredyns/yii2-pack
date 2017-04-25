061c66324d08d4e5037a4bf06091ce1f00a471f8
Fredy Nurman Saleh <email@fredyns.net>
2017-04-25 07:12:53 +0700

    naming helper

---
+++ src/helpers/NamingHelper.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace fredyns\pack\helpers;
+
+/**
+ * Description of NamingHelper
+ *
+ * @author fredy
+ */
+class NamingHelper
+{
+
+    /**
+     * trim unuser character
+     *
+     * @param string $name
+     * @return string
+     */
+    public static function trim($name)
+    {
+        return trim($name, ", \t\n\r\0\x0B");
+    }
+
+    /**
+     * alternate name format
+     *
+     * @param string $prefix
+     * @param string $name
+     * @param string $suffix
+     * @return string
+     */
+    public static function alternate($prefix = '', $name = '', $suffix = '')
+    {
+        $name = static::trim($name.', '.$suffix);
+
+        if (strlen($prefix) > 0) {
+            $name .= ' ('.$prefix.')';
+        }
+
+        return $name;
+    }
+
+    /**
+     * official name format
+     *
+     * @param string $prefix
+     * @param string $name
+     * @param string $suffix
+     * @return string
+     */
+    public static function official($prefix = '', $name = '', $suffix = '')
+    {
+        return static::trim($prefix.' '.$name.', '.$suffix);
+    }
+}
